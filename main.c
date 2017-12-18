6e90f876a2d6fae37e83cb4da8745daaf97786da
alexan95 <Manuel.Alexan.1927@student.uu.se>
2017-12-18 19:59:42 +0100

    Pontus CUnit commit

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "gc.h"
+#include <math.h>
 
 int main(int argc, char** argv)
 {
---
+++ test/test.c
@@ -0,0 +1,29 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <stdlib.h>
+#include <math.h>
+#include <CUnit/CUnit.h>
+#include <string.h>
+#include <string.h>
+//Import your test functions here
+
+
+void test_test() //This function should be placed in another file.
+{
+  
+  CU_ASSERT_EQUAL(strcmp("hej", "hej"), 0);
+}
+
+int main(int argc, char **argv) {
+  
+  CU_initialize_registry(); //Boilerplate
+
+  //New test suit begins
+  CU_pSuite tests = CU_add_suite("Unit tests sample", NULL, NULL); //Creats a bundle of tests (test suit) called tests.
+  CU_add_test(tests, "First unit test", test_test); // Adds a test to tests
+
+  //Add your test suits and unit tests here.
+  //The test functions should be placed in separate file the best thing would be one per module. Each module can have a seperate test suit to ease debugging.
+  
+  CU_basic_run_tests();//Run the tests
+}
