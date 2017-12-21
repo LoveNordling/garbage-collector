6a82870d764e65b581847fdbe1763563dbc3a7c1
Unknown <danzaricardo7@gmail.com>
2017-12-21 18:42:19 +0100

    Added bitwise functions
    Added function to set 2 least significant bits to 0 and func to check that 2 lsb's are 0

--- object.c
+++ object.c
@@ -1,13 +1,55 @@
 #include "object.h"
+#include <stdbool.h>
+#include <stdint.h>
 
+typedef struct object {
+    void *data; //forward adress efter objektet flyttats
+} object_t;
+        
+typedef struct header {
+    void *ptr; //TODO bitvektor  
+} header_t;
 
+//TODO
+//#define TO READ AND EDIT LAST TWO BITS
+
+// #define LAST TO BITS TO 0, shift right 2, shift left 2
 
 /** Ligga i egen modul Parser? Kanske lite onödigt. 
  **/
 
+typedef struct object {
+    void *data; //forward adress efter objektet flyttats
+} object_t;
+
+typedef struct header {
+    void *ptr; //TODO bitvektor
+} header_t;
+
+
+bool lsbs_are_zero(uintptr_t pointer)
+{
+  size_t integer = 3;
+  
+  return !(pointer & integer); 
+}
+
+uintptr_t byte_flipper_to_zero(uintptr_t pointer)
+{
+  size_t bitstoclear = 3;
+    if(lsbs_are_zero(pointer))
+        {
+          return pointer;
+        }
+    else
+        {
+          return pointer = pointer & ~bitstoclear;
+        }
+    
+}
+  
 size_t char_value(char c)
 {
-
     switch(c)
     {
     case 'i':
@@ -25,7 +67,6 @@ size_t char_value(char c)
     default:
         return 0;
     }
-  
 }
 
 size_t format_string_parser(char* layout)
@@ -46,9 +87,27 @@ size_t format_string_parser(char* layout)
 
 int main()
 {
+  int x;
+  int *num= &x;
+
+    /* Input number from user */
+    printf("Enter any number: ");
+    scanf("%d", &x);
+    printf("%d\n",*num);
+    /* If (num & 1) evaluates to 1 */
+    if(lsbs_are_zero(*num))
+        printf("LSB of %d is unset (0).\n", *num);
+    else
+        printf("LSB of %d is set (1).\n", *num);
+
+    printf("Original value: %d Flipped number:%d\n",*num, byte_flipper_to_zero(*num));
+ 
+
+  /*
     char layout[] = "**ii";
     printf("Amount of bytes that need allocating: %lu \n", format_string_parser(layout));
-    //printf("Storage size for : %d \n", sizeof(double));
-
+  */
+    printf("Storage size for : %d \n", sizeof(unsigned long long));
+    
     return 0;
 }
