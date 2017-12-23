6f39d4abc540f73bfd4cc14ea1e43e4021a9f2dd
LoveNordling <love_nordling@hotmail.com>
2017-12-23 14:42:02 +0100

    merge

--- gc.c
+++ gc.c
@@ -2,107 +2,8 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <math.h>
-<<<<<<< HEAD
-#include "cell.h"
-
-=======
-#include "root.h"
->>>>>>> manuel-root
-#ifdef _WIN32
-#include <windows.h>
-void* STACK_START_P;
-#endif
-
-typedef struct heap
-{
-<<<<<<< HEAD
-  bool unsafe_stack;
-  float gc_threshold;
-  void* data;
-  size_t size;
-  size_t cell_size;
-=======
-    bool unsafe_stack;
-    float gc_threshold;
-    size_t size;
-    void* data;
->>>>>>> manuel-root
-} heap_t;
-
-#define MIN_HEAP_SIZE sizeof(heap_t)
-#define MAX_HEAP_SIZE pow(2, 24)
-
 heap_t* h_init(size_t bytes, bool unsafe_stack, float gc_threshold)
 {
-<<<<<<< HEAD
-	//wrong argument checks
-	if (bytes < MIN_HEAP_SIZE)
-	{
-		bytes = MIN_HEAP_SIZE;
-		printf("minimum heap size is %d\n", MIN_HEAP_SIZE);
-	}
-	if (bytes > MAX_HEAP_SIZE)
-	{
-		bytes = MAX_HEAP_SIZE;
-		printf("maximum heap size is %d\n", MAX_HEAP_SIZE);
-	}
-	if (gc_threshold < 0)
-	{
-		gc_threshold = 0;
-		printf("threshold must be larger than 0%%\n");
-	}
-	if (gc_threshold > 1.0f)
-	{
-		gc_threshold = 1.0f;
-		printf("threshold must be less than 100%%\n");
-	}
-
-	//allocate memory for heap and its metadata
-	int b = bytes + sizeof(heap_t);
-	void* p = NULL;
-	int result = 0;
-        size_t size = b;
-	#ifdef _WIN32
-	p = __mingw_aligned_malloc(b, pow(2, 16));
-	#else
-	result = posix_memalign(&p, pow(2, 16), b);
-	#endif
-
-	if (p == NULL || result != 0)
-	{
-		printf("failed to allocate %d bytes\n", b);
-		return NULL;
-	}
-
-	//zero out memory
-	memset(p, 0, b);
-
-	//set struct pointer
-	heap_t* hp = p;
-	hp->unsafe_stack = unsafe_stack;
-	hp->gc_threshold = gc_threshold;
-
-	//set heap pointer
-	char* bp = p;
-	hp->data = bp + sizeof(heap_t);
-        hp->size = size;
-
-        //init cells
-        hp->cell_size = 200;
-        
-        for( void* cell = hp->data; cell < (void*)(hp + size); cell += (hp->cell_size + sizeof(cell)))
-          {
-            cell_new(cell);
-          }
-        for( void* cell = hp->data; cell < (void*)(hp + size); cell += (hp->cell_size + sizeof(cell)))
-          {
-            printf("%s ", cell_is_active(cell) ? "true" : "false");
-          }
-	printf("allocated %d bytes of memory at: %d\n", b, hp);
-
-	return hp;
-=======
-    //store start of stack
     #ifdef _WIN32
     STACK_START_P = __builtin_frame_address(0);
     #endif
@@ -111,19 +12,19 @@ heap_t* h_init(size_t bytes, bool unsafe_stack, float gc_threshold)
     if (bytes < MIN_HEAP_SIZE)
     {
         bytes = MIN_HEAP_SIZE;
-        printf("minimum heap size is %d\n", MIN_HEAP_SIZE);
+        printf("minimum heap size is %lu\n", MIN_HEAP_SIZE);
     }
-    if (bytes > MAX_HEAP_SIZE)
+    else if (bytes > MAX_HEAP_SIZE)
     {
         bytes = MAX_HEAP_SIZE;
-        printf("maximum heap size is %d\n", MAX_HEAP_SIZE);
+        printf("maximum heap size is %f\n", MAX_HEAP_SIZE);
     }
     if (gc_threshold < 0)
     {
         gc_threshold = 0;
         printf("threshold must be larger than 0%%\n");
     }
-    if (gc_threshold > 1.0f)
+    else if (gc_threshold > 1.0f)
     {
         gc_threshold = 1.0f;
         printf("threshold must be less than 100%%\n");
@@ -141,7 +42,7 @@ heap_t* h_init(size_t bytes, bool unsafe_stack, float gc_threshold)
 
     if (p == NULL || result != 0)
     {
-        printf("failed to allocate %d bytes\n", bytes);
+        printf("failed to allocate %lu bytes\n", bytes);
         return NULL;
     }
 
@@ -158,7 +59,7 @@ heap_t* h_init(size_t bytes, bool unsafe_stack, float gc_threshold)
     char* bp = p;
     hp->data = bp + sizeof(heap_t);
 
-    printf("allocated %d bytes of memory at: %d\n", bytes, hp);
+    printf("allocated %lu bytes of memory at: %p\n", bytes, hp);
 
     return hp;
 >>>>>>> manuel-root
@@ -191,7 +92,6 @@ void* h_alloc_data(heap_t* h, size_t bytes)
 
 size_t h_gc(heap_t* h)
 {
-    //TODO: pass in allocation map
     return scan_roots(h, NULL);
 }
 
