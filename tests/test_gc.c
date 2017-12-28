483a0839424e367ba88c97c3b7f908070f69e6d2
ponterik <Pontus.Eriksson.4631@student.uu.se>
2017-12-28 14:07:50 +0100

    Update test_gc.c

--- tests/test_gc.c
+++ tests/test_gc.c
@@ -8,16 +8,16 @@ void test_gc_init_delete()
 {
 
   
-  heap_t * h= h_init(500, true, 20);
-  CU_ASSERT_TRUE(h != NULL);
-  h_delete(h);
+    heap_t * h= h_init(500, true, 20);
+    CU_ASSERT_TRUE(h != NULL);
+    h_delete(h);
 
-  h= h_init(-33, true, 20);
-  CU_ASSERT_TRUE(h != NULL);
-  h_delete(h);
+    h= h_init(-33, true, 20);
+    CU_ASSERT_TRUE(h != NULL);
+    h_delete(h);
 
 
-  h= h_init(99999999999999, true, 20);
-  CU_ASSERT_TRUE(h != NULL);
-  h_delete(h);
+    h= h_init(99999999999999, true, 20);
+    CU_ASSERT_TRUE(h != NULL);
+    h_delete(h);
 }
