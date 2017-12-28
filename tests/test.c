7de28b91dfd16e89408c9a87d774fafb76baf5a7
ponterik <Pontus.Eriksson.4631@student.uu.se>
2017-12-28 14:07:19 +0100

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -12,24 +12,24 @@
 void test_test() //This function should be placed in another file.
 {
   
-  CU_ASSERT_EQUAL(strcmp("hej", "hej"), 0);
+   CU_ASSERT_EQUAL(strcmp("hej", "hej"), 0);
 }
 
 int main(int argc, char **argv)
 {
   
-  CU_initialize_registry(); //Boilerplate
+    CU_initialize_registry(); //Boilerplate
 
-  //New test suit begins
-  CU_pSuite tests = CU_add_suite("Unit tests sample", NULL, NULL); //Creats a bundle of tests (test suit) called tests.
-  CU_add_test(tests, "First unit test", test_test); // Adds a test to tests
+    //New test suit begins
+    CU_pSuite tests = CU_add_suite("Unit tests sample", NULL, NULL); //Creats a bundle of tests (test suit) called tests.
+    CU_add_test(tests, "First unit test", test_test); // Adds a test to tests
 
-  //Add your test suits and unit tests here.
-  //The test functions should be placed in separate file the best thing would be one per module. Each module can have a seperate test suit to ease debugging.
-  CU_pSuite test_gc = CU_add_suite("Testing gc.c", NULL, NULL);
-
-  CU_add_test(test_gc, "This only makes sense if you run valgrind", test_gc_init_delete);
+    //Add your test suits and unit tests here.
+    //The test functions should be placed in separate file the best thing would be one per module. Each module can have a seperate test suit to ease debugging.
+    CU_pSuite test_gc = CU_add_suite("Testing gc.c", NULL, NULL);
+  
+    CU_add_test(test_gc, "This only makes sense if you run valgrind", test_gc_init_delete);
 
   
-  CU_basic_run_tests();//Run the tests
+    CU_basic_run_tests();//Run the tests
 }
