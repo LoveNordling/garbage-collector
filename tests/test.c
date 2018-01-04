6e9885320a0e3b669378a8b9483a03cbc97c0eff
asdfelw <elwira.johansson.4420@student.uu.se>
2018-01-04 23:21:52 +0100

    Tests for bits-module.

--- tests/test.c
+++ tests/test.c
@@ -1,12 +1,9 @@
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
 #include <stdlib.h>
 #include <math.h>
-
-#include <CUnit/CUnit.h>
 #include <string.h>
-#include "test_memorymap.h"
-#include <string.h>
+#include "test_root.h"
+#include "test_integration.h"
+#include "test_bits.h"
 
 //Import your test functions here
 
@@ -14,25 +11,29 @@
 void test_test() //This function should be placed in another file.
 {
   
-
-    CU_ASSERT_EQUAL(strcmp("hej", "hej"), 0);
+  CU_ASSERT_EQUAL(strcmp("hej", "hej"), 0);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   
-    CU_initialize_registry(); //Boilerplate
+  CU_initialize_registry(); //Boilerplate
 
   //New test suit begins
-    CU_pSuite tests = CU_add_suite("Unit tests sample", NULL, NULL); //Creats a bundle of tests (test suit) called tests.
-    CU_add_test(tests, "First unit test", test_test); // Adds a test to tests
-
+  CU_pSuite tests = CU_add_suite("Unit tests sample", NULL, NULL); //Creats a bundle of tests (test suit) called tests.
+  CU_add_test(tests, "First unit test", test_test); // Adds a test to tests
+  CU_add_test(tests, "testing safe pointer check", test_is_pointer_to_heap);
+  //CU_add_test(tests, "Integration test", test_gc);
   //Add your test suits and unit tests here.
   //The test functions should be placed in separate file the best thing would be one per module. Each module can have a seperate test suit to ease debugging.
 
-    CU_pSuite memmap = CU_add_suite("Testing memmap", NULL, NULL);
-    CU_add_test(memmap, "", test_memorymap);
+  CU_pSuite bit_tests = CU_add_suite("Bit module", NULL, NULL); //Creats a bundle of tests (test suit) called tests.
+  CU_add_test(bit_tests, "Test set a bit to at a certain index", test_set_bit);
+  CU_add_test(bit_tests, "Test get least significant bits", test_get_lsbs);
+  CU_add_test(bit_tests, "Test set least significant bits", test_set_lsbs);
+  CU_add_test(bit_tests, "Test lsbs are zero", test_lsbs_are_zero);
+  CU_add_test(bit_tests, "Test lsbs to zero", test_lsbs_to_zero);
+  CU_add_test(bit_tests, "Test set most significant bit", test_set_msb);
+  CU_add_test(bit_tests, "Test get most significant bit", test_get_msb);
   
-
-  
-    CU_basic_run_tests();//Run the tests
+  CU_basic_run_tests();//Run the tests
 }
