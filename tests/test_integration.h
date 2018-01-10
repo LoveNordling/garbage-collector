0c365525da9100c0aa5f5709a9b2a4d88eb06847
LoveNordling <love_nordling@hotmail.com>
2018-01-10 23:17:15 +0100

    Prepared to merge master

--- tests/test_integration.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_INTEGRATION_H
-#define TEST_INTEGRATION_H
-
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
-
-void test_gc();
-
-#endif
--- tests/test_root.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_ROOT_H
-#define TEST_ROOT_H
-
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
-
-void test_is_pointer_to_heap();
-
-#endif /* TEST_ROOT_H */
